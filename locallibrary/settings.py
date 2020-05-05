033b043d1698661606300ac835ae1e0b7a47a4e9
KonstErz <konstantin.yerzin@gmail.com>
2020-05-05 22:12:44 +0700

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*pjadtm=spbvm7i38+mf*@=ife(tji
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pacific-atoll-84292.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
